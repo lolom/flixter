f00b42d7a77bd332e230f941020f68b532cba968
Lorena <lmlorem@gmail.com>
2017-09-08 15:55:32 +0000

    Adding Validation

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -6,17 +6,21 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def create
-      @course = current_user.courses.create(course_params)
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
       redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
   def show
     @course = Course.find(params[:id])
   end
 
-    private
+  private
 
-    def course_params
-      params.require(:course).permit(:title, :description, :cost)
-    end
+  def course_params
+    params.require(:course).permit(:title, :description, :cost)
+  end
 end
